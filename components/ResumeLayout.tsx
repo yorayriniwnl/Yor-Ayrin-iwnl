638d8b49802e275fbff6c5eb37106934e301c48f
YorAyriniwnl <yorayriniwnl@gmail.com>
2026-04-20 01:17:57 +0530

    Prune unreachable legacy code

--- components/ResumeLayout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ResumeLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <article className="rounded-[2rem] border border-slate-200 bg-white p-8 shadow-2xl shadow-slate-950/10 print:rounded-none print:border-0 print:p-0 print:shadow-none">
-      {children}
-    </article>
-  )
-}
